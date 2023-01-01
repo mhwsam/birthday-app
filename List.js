1cfc66d80b336aa7b8cc2067ae8904a979734cf6
mhwsam <117499930+mhwsam@users.noreply.github.com>
2023-01-01 16:59:04 +0600

    Add files via upload

---
+++ List.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const List = ({ manush }) => {
+  return (
+    <>
+      {manush.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className='person'>
+           
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;
