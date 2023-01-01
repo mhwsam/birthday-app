3b4ea36d5f9edba9b06e47c6c9caa5747cf9f9a2
mhwsam <117499930+mhwsam@users.noreply.github.com>
2023-01-01 16:59:39 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        id:1,
+        name: 'Mohammad Mahadi',
+        age:28
+
+    },
+    {
+        id:2,
+        name: 'Mohammad Tazul',
+        age:29
+
+    },
+    {
+        id:3,
+        name: 'Mohammad Mahbub',
+        age:26
+
+    },
+    {
+        id:4,
+        name: 'Mohammad Rabbi',
+        age:27
+
+    },
+    {
+        id:5,
+        name: 'Mohammad Anan',
+        age:25
+
+    },
+    {
+        id:6,
+        name: 'Mohammad Imam',
+        age:34
+
+    },
+    {
+        id:7,
+        name: 'Mohammad Ahsan',
+        age:40
+    }
+
+]
